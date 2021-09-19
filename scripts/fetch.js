17d04b670180eb64f7cee31adcc31e8d4240a05f
ChristopherPerrault <christopherperrault@hotmail.com>
2021-09-19 17:30:36 -0400

    WIP

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -14,10 +14,27 @@ async function sendApiRequest() {
     `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=pizza`
   );
   //^ after q should be what user input is, above it's just pizza for now
+  // can't figure out how to make it user input
   console.log(response);
   let data = await response.json();
   console.log(data);
+  useApiData(data);
 }
 
 //function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
-function useApiData(data) {}
+function useApiData(data) {
+  document.querySelector("#recipe-content").innerHTML = `
+    <div class="card" style="width: 18rem;">
+    <img src="${data.hits[0].recipe.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${data.hits[0].recipe.label}</h5>
+    <p class="card-text">Source:&nbsp${JSON.stringify(
+      data.hits[0].recipe.source
+    )}</p>
+    <a href="${
+      data.hits[0].recipe.url
+    }" class="btn btn-primary">Visit Source</a>
+    </div>
+    </div>
+    `;
+}
